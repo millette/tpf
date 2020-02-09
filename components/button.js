3e519e935ff59d2f2bcabb16c7e2a2d00622cdda
Robin Millette <robin@millette.info>
2020-02-09 17:47:15 -0500

    Use theme-ui in component.

--- components/button.js
+++ components/button.js
@@ -1,12 +1,18 @@
+/** @jsx jsx */
+
+import { jsx } from "theme-ui"
+
 export default ({ children }) => (
   <button
-    style={{
+    sx={{
       borderRadius: "3px",
+      /*
       border: "1px solid black",
-      color: "black",
+      color: "pink",
       padding: "0.5em 1em",
       cursor: "pointer",
       fontSize: "1.1em",
+      */
     }}
   >
     {children}
