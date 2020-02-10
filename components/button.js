b981c14b982ddd8e4197c08fc8c09addd0116259
Robin Millette <robin@millette.info>
2020-02-10 11:41:35 -0500

    Remove button component style.

--- components/button.js
+++ components/button.js
@@ -2,19 +2,4 @@
 
 import { jsx } from "theme-ui"
 
-export default ({ children }) => (
-  <button
-    sx={{
-      borderRadius: "3px",
-      /*
-      border: "1px solid black",
-      color: "pink",
-      padding: "0.5em 1em",
-      cursor: "pointer",
-      fontSize: "1.1em",
-      */
-    }}
-  >
-    {children}
-  </button>
-)
+export default ({ children }) => <button>{children}</button>
