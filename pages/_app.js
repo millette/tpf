153f3e31cd5e05f8b372efbb2311b00a12aef666
Robin Millette <robin@millette.info>
2020-02-10 14:45:30 -0500

    Use theme.

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,9 @@
-/** @jsx jsx */
-
 // npm
-import { jsx, Styled, Button, ThemeProvider } from "theme-ui"
-// import { ColorModeProvider } from '@theme-ui/color-modes'
-import theme1 from "@theme-ui/preset-polaris"
+import { Styled, Button, ThemeProvider, InitializeColorMode } from "theme-ui"
 import Link from "next/link"
 
 // self
-// import Cols from "../components/cols.js"
+import theme from "../theme.js"
 
 const mdComponents = {
   Button,
@@ -23,70 +19,7 @@ const mdComponents = {
     ),
 }
 
-const theme = {
-  ...theme1,
-  // useColorSchemeMediaQuery: true,
-  buttons: {
-    secondary: {
-      cursor: "pointer",
-      fontWeight: "bold",
-      color: "white",
-      bg: "red",
-      "&:hover": {
-        bg: "dark",
-      },
-    },
-  },
-}
-
-/*
-theme1.useColorSchemeMediaQuery = true
-
-// theme1.styles
-
-if (theme1.styles) {
-  theme1.styles.buttons = {
-    primary: {
-      fontWeight: 'bold',
-      color: 'white',
-      bg: 'primary',
-      '&:hover': {
-        bg: 'dark',
-    },
-
-    secondary: {
-      cursor: "pointer",
-      fontWeight: 'bold',
-      color: 'white',
-      bg: 'secondary',
-      '&:hover': {
-        bg: 'dark',
-    },
-  }
-}
-}
-}
-
-console.log("THEME1", Object.keys(theme1))
-console.log("THEME1-styles", theme1.styles && Object.keys(theme1.styles))
-*/
-
-/*
-const theme = merge(theme1, {
-  useColorSchemeMediaQuery: true,
-})
-
-console.log("THEME", Object.keys(theme))
-*/
-
-/*
-
-
-      <ColorModeProvider>
-        <Cols />
-      </ColorModeProvider>
-
-*/
+// <InitializeColorMode />
 
 export default ({ Component, pageProps }) => {
   return (
