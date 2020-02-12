8f813dad64500534516aca3f53f73fcdf28226f3
Robin Millette <robin@millette.info>
2020-02-12 18:59:23 -0500

    Add home page link to all pages (temporarely).

--- components/cols.js
+++ components/cols.js
@@ -1,16 +1,22 @@
 // npm
+import Link from "next/link"
 import { useColorMode, Button } from "theme-ui"
 
 const Cols = () => {
   const [colorMode, setColorMode] = useColorMode()
   return (
-    <Button
-      onClick={(e) => {
-        setColorMode(colorMode === "default" ? "dark" : "default")
-      }}
-    >
-      Toggle {colorMode === "default" ? "Dark" : "Light"}
-    </Button>
+    <>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Button
+        onClick={(e) => {
+          setColorMode(colorMode === "default" ? "dark" : "default")
+        }}
+      >
+        Toggle {colorMode === "default" ? "Dark" : "Light"}
+      </Button>
+    </>
   )
 }
 
