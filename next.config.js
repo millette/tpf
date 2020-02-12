da33fa8e4a757a341c94d2c1be979cd61d013e09
Robin Millette <robin@millette.info>
2020-02-12 15:56:10 -0500

    Add optimized-images to next config.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 "use strict"
 
 // npm
+const withOptimizedImages = require("next-optimized-images")
 const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
 })
 
-module.exports = withMDX({
-  pageExtensions: ["js", "mdx"],
-})
+module.exports = withOptimizedImages(
+  withMDX({
+    pageExtensions: ["js", "mdx"],
+  })
+)
