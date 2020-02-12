708deb77bdea54272a68e6958cfb8c68941038ec
Robin Millette <robin@millette.info>
2020-02-12 17:19:16 -0500

    Preset images sizes in config.

--- next.config.js
+++ next.config.js
@@ -8,6 +8,9 @@ const withMDX = require("@next/mdx")({
 
 module.exports = withOptimizedImages(
   withMDX({
+    responsive: {
+      sizes: [600, 1000, 2000],
+    },
     pageExtensions: ["js", "mdx"],
   })
 )
