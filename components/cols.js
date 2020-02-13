8f7beb141de2f2ba86152bde4ed935c9b7583558
Robin Millette <robin@millette.info>
2020-02-13 14:57:06 -0500

    Remove extraneous home page link.

--- components/cols.js
+++ components/cols.js
@@ -1,22 +1,16 @@
 // npm
-import Link from "next/link"
 import { useColorMode, Button } from "theme-ui"
 
 const Cols = () => {
   const [colorMode, setColorMode] = useColorMode()
   return (
-    <>
-      <Link href="/" as={process.env.BACKEND_URL + "/"}>
-        <a>Home</a>
-      </Link>
-      <Button
-        onClick={(e) => {
-          setColorMode(colorMode === "default" ? "dark" : "default")
-        }}
-      >
-        Toggle {colorMode === "default" ? "Dark" : "Light"}
-      </Button>
-    </>
+    <Button
+      onClick={(e) => {
+        setColorMode(colorMode === "default" ? "dark" : "default")
+      }}
+    >
+      Toggle {colorMode === "default" ? "Dark" : "Light"}
+    </Button>
   )
 }
 
